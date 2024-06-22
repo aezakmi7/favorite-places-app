a4acd4a84e5f6733a8e109c49ea751cf1f33df59
Mykola Herasymenko <nicksuper2000@gmail.com>
2024-06-22 13:54:24 -0400

    "add image" feature

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,4 +1,5 @@
 import 'package:favorite_places_app/providers/user_places.dart';
+import 'package:favorite_places_app/widgets/image_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -46,6 +47,7 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
               ),
               controller: _titleController,
             ),
+            ImageInput(),
             const SizedBox(height: 16),
             Row(
               children: [
