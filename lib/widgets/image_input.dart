48a07c47eb6c50e4c63492dba2531b2f25571fb3
Mykola Herasymenko <nicksuper2000@gmail.com>
2024-06-28 16:08:16 -0400

    implemented image picker

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -8,16 +9,26 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  void _takePicture() async {
+    final imagePicker = ImagePicker();
+    final pickedImage = await imagePicker
+        .pickImage(source: ImageSource.camera, maxWidth: 600)
+        .then((image) {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black, width: 1.0),
+      ),
       height: 250,
       width: double.infinity,
       alignment: Alignment.center,
       child: TextButton.icon(
         icon: const Icon(Icons.camera),
         label: const Text('Take a picture'),
-        onPressed: () {},
+        onPressed: _takePicture,
       ),
     );
   }
